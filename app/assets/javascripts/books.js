c4a6ee9ddf52851dcdd552e8acdefdac92bd5147
Sineong <sineong@wafflestudio.com>
2013-11-15 16:21:12 +0900

    Add search and tried to toggle-slide book-content

---
+++ app/assets/javascripts/books.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+  $("td#booktitle").click(function(){
+    $(this).parent().parent().children(".book-content").slideToggle("slow");
+
+  }); 
+  $(".available.bookClass").click(function(){
+    var r = confirm("이 책을 빌리시겠습니까?")
+    if (r){
+      var bookId = $(this).attr('book-id')
+      $.get('/lend/' + bookId, function(data) { 
+        if (data.status == "OK")
+          {   
+            alert("대출하였습니다")
+            $('button[book-id='+bookId+']').removeClass('available').addClass('reservation').html('대여불가') 
+          }   
+          else
+            alert("대출권수를 초과하였습니다")
+      }); 
+
+
+    }   
+
+
+  }); 
+  $(".reservation.bookClass").click(function(){
+    var r = confirm("이 책을 예약하시겠습니까?")
+    if (r){
+      alert("예약!")
+
+    }   
+    else{
+      alert("안예약!")
+
+    }   
+
+
+  }); 
+
+
+});
+
